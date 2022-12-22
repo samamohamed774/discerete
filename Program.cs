bde2afe4b8f45d341920a55809b22ab7e9d20c73
samamohamed774 <114769223+samamohamed774@users.noreply.github.com>
2022-12-22 13:23:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project2
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int z = 0;
+            Console.WriteLine("Please enter the start number");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the end number");
+            int n2 = int.Parse(Console.ReadLine());
+            
+            for (int i = n1; i <= n2; i++)
+            {
+                z = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                        z += j; 
+
+                }
+
+                if (z == i)
+                {
+                    Console.WriteLine(i + " is perfect");
+                }
+
+            }
+
+        }
+    }
+}
